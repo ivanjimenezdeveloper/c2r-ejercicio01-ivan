c3f629bd3edb95862b4d3c9fd7026ea992e7b7d9
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-06-10 11:40:34 +0200

    añadida posicion aleatoria con over

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState } from "react";
+
 function App() {
-  const clasesGlobico = "fab fa-fly";
+  const clasesGlobico = "fab fa-fly p-absolute";
+  let timer;
+  const posicionYAleatoria = () =>
+    Math.floor(Math.random() * window.innerHeight);
+  const posicionXAleatoria = () =>
+    Math.floor(Math.random() * window.innerWidth);
+
+  const [ejesPosicion, setEjesPosicion] = useState({
+    x: posicionXAleatoria(),
+    y: posicionYAleatoria(),
+  });
+
+  const posicionAleatoria = () => {
+    clearTimeout(timer);
+    timer = setTimeout(
+      () =>
+        setEjesPosicion({ y: posicionYAleatoria(), x: posicionXAleatoria() }),
+      500
+    );
+  };
 
   return (
     <div className="App">
-      <i className={clasesGlobico}></i>
+      <i
+        className={clasesGlobico}
+        style={{ top: ejesPosicion.y, left: ejesPosicion.x }}
+        onMouseOver={posicionAleatoria}
+      ></i>
     </div>
   );
 }
